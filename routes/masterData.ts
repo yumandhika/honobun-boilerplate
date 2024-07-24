7a72a34eba342abd0af7b1a99d9ae1f38aa74251
yumandhika <yuma.jawa48@gmail.com>
2024-07-25 01:39:04 +0700

    add api master constant

--- routes/masterData.ts
+++ routes/masterData.ts
@@ -1,7 +1,17 @@
 import { Hono } from 'hono'
 import { getListCity, getListDistrict, getListProvince } from '../controllers/masterData'
+import { successResponse } from '../utils/helpers'
+import { orderStatus } from '../constants/orderStatus'
+import { paymentType } from '../constants/paymentType'
+import { roles } from '../constants/roles'
+import { serviceType } from '../constants/serviceType'
 
 export const masterDataRoute = new Hono()
   .get('/cities', getListCity)
   .get('/districts', getListDistrict)
   .get('/provinces', getListProvince)
+
+  .get('/order-status', async (c) => {return successResponse(c, orderStatus)})
+  .get('/payment-type', async (c) => {return successResponse(c, paymentType)})
+  .get('/roles', async (c) => {return successResponse(c, roles)})
+  .get('/service-type', async (c) => {return successResponse(c, serviceType)})

6f78c094af786831a6b43ef51996f48c9976bd2c
yumandhika <yuma.jawa48@gmail.com>
2024-07-26 10:00:15 +0700

    cors

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
+import { cors } from 'hono/cors'
 import { Routes } from './routes'
 import { customLogger, customTimeoutException } from './utils/custom-logger'
 import { timeout } from 'hono/timeout'
@@ -8,6 +9,7 @@ import { swaggerUI } from '@hono/swagger-ui'
 const app = new Hono()
 
 app.use('*', logger(customLogger)) // Logger all request
+app.use('*', cors()) // Enable CORS for all routes
 
 app.get('/', swaggerUI({ url: '/doc' }))
 app.use('/api', timeout(60000, customTimeoutException))

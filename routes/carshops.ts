3af9db2dfa5a03c6116faa1da685b27d2b6d52ec
yumandhika <yuma.jawa48@gmail.com>
2024-07-28 15:10:25 +0700

    add cashop phone

--- routes/carshops.ts
+++ routes/carshops.ts
@@ -1,8 +1,8 @@
 import { Hono } from 'hono'
 import { updateCustomerCar } from '../controllers/cars';
-import { createCarShops, getListCarShops } from '../controllers/carShops';
+import { createCarShops, getListCarShops, updateCarShops } from '../controllers/carShops';
 
 export const carShopsRoute = new Hono()
   .get('/', getListCarShops)
   .post('/', createCarShops)
-  .put('/:id', updateCustomerCar)
+  .put('/:id', updateCarShops)
